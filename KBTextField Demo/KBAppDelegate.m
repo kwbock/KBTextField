ac93ae06ccde83accc59e5d419a60a0eede017f0
Kyle Bock <kylewbock@gmail.com>
2012-11-05 09:32:38 -0500

    Initial Commit

---
+++ KBTextField Demo/KBAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  KBAppDelegate.m
+//  KBTextField Demo
+//
+//  Created by Kyle Bock on 11/5/12.
+//  Copyright (c) 2012 Kyle Bock. All rights reserved.
+//
+
+#import "KBAppDelegate.h"
+
+@implementation KBAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ KBTextField Demo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  KBTextField Demo
+//
+//  Created by Kyle Bock on 11/5/12.
+//  Copyright (c) 2012 Kyle Bock. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
