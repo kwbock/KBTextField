4b11941ea9a038a8fb6a76a3f380c39606e3a86c
Kyle Bock <kylewbock@gmail.com>
2012-11-05 15:15:18 -0500

    added _cFlags.vCentered = 1 to awakeFromNib. this is dirty and you aren't supposed to access _cFlags but we had text ghosting so until we find a better solution we shall use cflags.

--- KBtextField/KBTextFieldCell.m
+++ KBtextField/KBTextFieldCell.m
@@ -10,6 +10,14 @@
 
 @implementation KBTextFieldCell
 
+- (void)awakeFromNib {
+    [self setDrawsBackground:NO];
+    
+    // dirty and you aren't supposed to do this
+    // using this until we find a better solution
+     _cFlags.vCentered = 1;
+}
+
 - (id) init {
     self = [super init];
     if (self) {
@@ -19,19 +27,52 @@
     return self;
 }
 
-- (void)awakeFromNib {
-    [self setDrawsBackground:NO];
-    
+#pragma mark - Editing Methods
+//- (NSRect)titleRectForBounds:(NSRect)theRect {
+//    NSRect titleFrame = [super titleRectForBounds:theRect];
+//    NSSize titleSize = [[self attributedStringValue] size];
+//    titleFrame.origin.y = theRect.origin.y + (theRect.size.height - titleSize.height) / 2.0;
+//    return titleFrame;
+//}
+//
+//- (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
+//    NSRect titleRect = [self titleRectForBounds:cellFrame];
+//    [[self attributedStringValue] drawInRect:titleRect];
+//}
+//- (NSRect)titleRectForBounds:(NSRect)theRect {
+//    NSRect titleFrame = [super titleRectForBounds:theRect];
+//    NSSize titleSize = [[self attributedStringValue] size];
+//    
+//    // modified:
+//    theRect.origin.y += (theRect.size.height - titleSize.height)/2.0 - 2;
+//    return theRect;
+//}
+//
+
+- (void)selectWithFrame:(NSRect)aRect inView:(NSView *)controlView editor:(NSText *)textObj delegate:(id)anObject start:(NSInteger)selStart length:(NSInteger)selLength
+{
+	//aRect = [self titleRectForBounds:aRect];
+	[super selectWithFrame:aRect inView:controlView editor:textObj delegate:anObject start:selStart length:selLength];
 }
 
-- (void)drawWithFrame:(NSRect)frame inView:(NSView *)controlView {
-    [NSGraphicsContext saveGraphicsState];
-    NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:frame xRadius:7.0f yRadius:7.0f];
+- (void)editWithFrame:(NSRect)aRect inView:(NSView *)controlView editor:(NSText *)textObj delegate:(id)anObject event:(NSEvent *)theEvent
+{
+	//aRect = [self titleRectForBounds:aRect];
+	[super editWithFrame:aRect inView:controlView editor:textObj delegate:anObject event:theEvent];
+}
 
+#pragma mark - Drawing Methods
+
+- (void)drawWithFrame:(NSRect)frame inView:(NSView *)controlView {
+    CGFloat borderRadius = 4.0f;
+    
+    [NSGraphicsContext saveGraphicsState];
+    NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:frame xRadius:borderRadius yRadius:borderRadius];
+    
     [[NSColor grayColor] setFill];
     [path fill];
     
-    [[NSBezierPath bezierPathWithRoundedRect:NSInsetRect(frame, 1.0f, 1.0f) xRadius:6.0f yRadius:6.0f] addClip];
+    [[NSBezierPath bezierPathWithRoundedRect:NSInsetRect(frame, 1.0f, 1.0f) xRadius:borderRadius - 1 yRadius:borderRadius - 1] addClip];
     [[NSColor whiteColor] setFill];
     NSRectFillUsingOperation(frame, NSCompositeSourceOver);
     [NSGraphicsContext restoreGraphicsState];
@@ -39,17 +80,15 @@
     [self drawInteriorWithFrame:frame inView:controlView];
 }
 
-
-- (void)selectWithFrame:(NSRect)aRect inView:(NSView *)controlView editor:(NSText *)textObj delegate:(id)anObject start:(NSInteger)selStart length:(NSInteger)selLength
-{
-	aRect = [self titleRectForBounds:aRect];
-	[super selectWithFrame:aRect inView:controlView editor:textObj delegate:anObject start:selStart length:selLength];
+// changes focus ring radius to match that of the text cell
+- (void)drawFocusRingMaskWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
+    [NSGraphicsContext saveGraphicsState];
+    NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:cellFrame xRadius:4.0f yRadius:4.0f];
+    [path addClip];
+    [path fill];
+    [NSGraphicsContext restoreGraphicsState];
+    [super drawFocusRingMaskWithFrame:cellFrame inView:controlView];
 }
 
-- (void)editWithFrame:(NSRect)aRect inView:(NSView *)controlView editor:(NSText *)textObj delegate:(id)anObject event:(NSEvent *)theEvent
-{
-	aRect = [self titleRectForBounds:aRect];
-	[super editWithFrame:aRect inView:controlView editor:textObj delegate:anObject event:theEvent];
-}
 
 @end
